1cbd889be0b5d00c218f32b808bd654a0b42f61b
Rahul Gopinath <vrthra@users.noreply.github.com>
2021-06-24 11:00:49 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         "typer",
         "pygments",
         "rich",
-        "kernel_driver>=0.0.6",
+        "kernel_driver",
     ],
     dependency_links=[
         'git+ssh://git@github.com/vrthra-forks/kernel_driver.git#egg=kernel_driver-0.0.6'
